a55beed396c97b11857670406967a04f89163c46
Isabel Rial <isabel.rial@gmail.com>
2016-09-10 21:12:44 -0400

    video player component

--- app/index.js
+++ app/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var videoSource = 'http://grochtdreis.de/fuer-jsfiddle/video/sintel_trailer-480.mp4';
+
+var VideoPlayer = React.createClass({
+	getInitialState: function() {
+		return {
+			src: this.props.src
+		};
+	},
+
+	render: function() {
+		return (
+			<video controls preload="metadata" style={{maxWidth: '100%'}}>
+      			<source src={this.props.src}  type='video/mp4' />
+    		</video>
+		);
+	}
+});
+
 ReactDOM.render(
-  <h1>Hello World</h1>,
-  document.getElementById('react-root')
+  <VideoPlayer src={videoSource}/>,
+  document.getElementById('video-clipper')
 );
