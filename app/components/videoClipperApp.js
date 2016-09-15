5cf5de20120220156902c2091ee523d961d8f3d2
Isabel Rial <isabel.rial@gmail.com>
2016-09-15 15:19:05 -0400

    organize component dependencies

--- app/components/videoClipperApp.js
+++ app/components/videoClipperApp.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {VideoPlayer} from './videoPlayer'
+import {VideoClipForm} from './VideoClipForm'
 
 export const VideoClipperApp = React.createClass({
 	getInitialState: function() {
--- app/index.js
+++ app/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {VideoPlayer} from './components/videoPlayer'
-import {VideoClipForm} from './components/VideoClipForm'
 import {VideoClipperApp} from './components/VideoClipperApp'
 
 var update = require('react-addons-update');
